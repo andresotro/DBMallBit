594ae70753ae3f173cfde24c1ec090beb8c14597
Apfol <andresfabi90@gmail.com>
2017-10-05 11:09:15 -0500

    Creación Clases y Servlet vendedor

---
+++ SQL/Proyecto código sql.sql
@@ -0,0 +1,30 @@
+create database proyecto;
+use proyecto;
+
+create table cliente( IDCliente int(11) not null auto_increment, Nombre varchar(50) not null, Apellido varchar(50) not null,FechaNacimiento date not null, Correo varchar(100) not null, Identificacion int(11) not null unique, IDGenero int(11), Telefono int(10) not null, Primary Key(IDCliente));
+create table genero(IDGenero int(11) not null, Tipo varchar(100) not null, Primary Key(IDGenero));
+create table compra(IDCompra int(11) not null, IDPago int(11), IDCliente int(11), IDProducto int(11), Primary Key(IDCompra));
+create table producto(IDProducto int(11) not null, Nombre varchar(100) not null, Precio int not null, IDLocal int(11), IDCategoria int(11), Descripcion varchar(100) not null, Primary Key(IDProducto));
+create table categoria(IDCategoria int(11) not null, Categoria varchar(100) not null, Primary Key(IDCategoria));
+create table local(IDLocal int(11) not null, Nombre varchar(100) not null, IDVendedor int(11), Descripcion varchar(100) not null, Primary Key(IDLocal));
+create table vendedor(IDVendedor int(11) not null, Nombre varchar(100) not null, FechaNacimiento date not null, Email varchar(100) not null, Identificacion int(11) not null unique, Telefono int(10) not null, Primary Key(IDVendedor));
+create table envio(IDEnvio int(11) not null, FechaEnvio date not null, Descripcion varchar(100) not null, IDEstado int(11), IDCompra int(11), IDVendedor int(11), Primary Key(IDEnvio));
+create table estado(IDEstado int(11) not null, Estado varchar(100) not null, Primary Key(IDEstado));
+create table pago(IDPago int(11) not null, FechaPago date not null, Primary Key(IDPago));
+create table administrador(IDAdministrador int(11) not null, Nombre varchar(100) not null, Identificacion int(11) not null unique, FechaNacimiento date not null, Email varchar(100) not null, Telefono int(10) not null, Primary Key(IDAdministrador));
+create table premio(IDPremio int(11) not null, Nombre varchar(100) not null, Descripcion varchar(100) not null, IDAdministrador int(11), Primary Key(IDPremio));
+create table clientepremio(IDPremio int(11), IDCliente int(11), Primary Key(IDPremio, IDCliente));
+
+alter table producto add foreign key(IDLocal) references local(IDLocal);
+alter table producto add foreign key(IDCategoria) references categoria(IDCategoria);
+alter table local add foreign key(IDVendedor) references vendedor(IDVendedor);
+alter table compra add foreign key(IDPago) references pago(IDPago);
+alter table compra add foreign key(IDCliente) references cliente(IDCliente);
+alter table compra add foreign key(IDProducto) references producto(IDProducto);
+alter table envio add foreign key(IDEstado) references estado(IDEstado);
+alter table envio add foreign key(IDCompra) references compra(IDCompra);
+alter table envio add foreign key(IDVendedor) references vendedor(IDVendedor);
+alter table cliente add foreign key(IDGenero) references genero(IDGenero);
+alter table premio add foreign key(IDAdministrador) references administrador(IDAdministrador);
+alter table clientepremio add foreign key(IDCliente) references cliente(IDCliente);
+alter table clientepremio add foreign key(IDPremio) references premio(IDPremio);
--- src/java/com/mallbit/cliente/ModeloCliente.java
+++ src/java/com/mallbit/cliente/ModeloCliente.java
@@ -9,72 +9,70 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 
 public class ModeloCliente {
-		
-		public List<Cliente> obtenerClientesDB() throws Exception {
 
-	        List<Cliente> clientes = new ArrayList<>();
+    public List<Cliente> obtenerClientesDB() throws Exception {
 
-	        Connection connection;
-	        Statement statement;
-	        ResultSet resultSet;
+        List<Cliente> clientes = new ArrayList<>();
 
-	        //Establecer la conexi�n
-	        connection = ConexionDB.conectar();
+        Connection connection;
+        Statement statement;
+        ResultSet resultSet;
 
-	        //Crear sentencia SQL y statement
-	        String sentenciaSQL = "SELECT * FROM cliente";
-	        statement = connection.createStatement();
+        //Establecer la conexi�n
+        connection = ConexionDB.conectar();
 
-	        //Ejecutar SQL y guardar valores de consulta en resultSet
-	        resultSet = statement.executeQuery(sentenciaSQL);
+        //Crear sentencia SQL y statement
+        String sentenciaSQL = "SELECT * FROM cliente";
+        statement = connection.createStatement();
 
-	        //Recorrer resultador de la sentencia
-	        while (resultSet.next()) {
-	            int id = resultSet.getInt("IDCliente"); 
-	            String nombre = resultSet.getString("Nombre");
-	            String apellido = resultSet.getString("Apellido");
-	            String correo = resultSet.getString("Correo");
-                    long identificacion = resultSet.getLong("identificacion");
-                    long telefono = resultSet.getLong("telefono");
-	            String usuario = resultSet.getString("Usuario");
-	            String contraseña = resultSet.getString("Password");
-	            Date fechaNacimiento = resultSet.getDate("FechaNacimiento");
-	            int idGenero = resultSet.getInt("IDGenero");
+        //Ejecutar SQL y guardar valores de consulta en resultSet
+        resultSet = statement.executeQuery(sentenciaSQL);
 
-	            clientes.add(new Cliente(id, nombre, apellido, correo, identificacion, telefono, usuario, contraseña, fechaNacimiento, idGenero));
+        //Recorrer resultador de la sentencia
+        while (resultSet.next()) {
+            int id = resultSet.getInt("IDCliente");
+            String nombre = resultSet.getString("Nombre");
+            String apellido = resultSet.getString("Apellido");
+            String correo = resultSet.getString("Correo");
+            long identificacion = resultSet.getLong("identificacion");
+            long telefono = resultSet.getLong("telefono");
+            String usuario = resultSet.getString("Usuario");
+            String contraseña = resultSet.getString("Password");
+            Date fechaNacimiento = resultSet.getDate("FechaNacimiento");
+            int idGenero = resultSet.getInt("IDGenero");
 
-	        }
-	        return clientes;
-	    }
+            clientes.add(new Cliente(id, nombre, apellido, correo, identificacion, telefono, usuario, contraseña, fechaNacimiento, idGenero));
 
-	    public void agregarClienteDB(Cliente cliente) throws SQLException {
-	        
-	        Connection connection;
-	        PreparedStatement preparedStatement;
+        }
+        return clientes;
+    }
 
-	        //Establecer la conexi�n
-	        connection = ConexionDB.conectar();
+    public void agregarClienteDB(Cliente cliente) throws SQLException {
 
-	        //Crear sentencia SQL y statement
-	        String sentenciaSQL = "INSERT INTO cliente "
-	                + "(Nombre,Apellido,Correo,Identificacion,Telefono,Usuario,Password,FechaNacimiento,IDGenero) VALUES (?,?,?,?,?,?,?,?,?)";
-	        preparedStatement = connection.prepareStatement(sentenciaSQL);
-	        
-	        //Pasar valores del objeto cliente a la sentenciaSQL
-	        preparedStatement.setString(1, cliente.getNombre());
-	        preparedStatement.setString(2, cliente.getApellido());
-	        preparedStatement.setString(3, cliente.getCorreo());
-                preparedStatement.setLong(4, cliente.getIdentificacion());
-                preparedStatement.setLong(5, cliente.getTelefono());
-	        preparedStatement.setString(6, cliente.getUsuario());
-	        preparedStatement.setString(7, cliente.getContraseña());
-	        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
-	        preparedStatement.setString(8, formatDate.format(cliente.getFechaNacimiento()));
-	        preparedStatement.setInt(9, cliente.getIdGenero());
-	        
-	        preparedStatement.execute();
-	    }
-	    
-	  
-	
+        Connection connection;
+        PreparedStatement preparedStatement;
+
+        //Establecer la conexi�n
+        connection = ConexionDB.conectar();
+
+        //Crear sentencia SQL y statement
+        String sentenciaSQL = "INSERT INTO cliente "
+                + "(Nombre,Apellido,Correo,Identificacion,Telefono,Usuario,Password,FechaNacimiento,IDGenero) VALUES (?,?,?,?,?,?,?,?,?)";
+        preparedStatement = connection.prepareStatement(sentenciaSQL);
+
+        //Pasar valores del objeto cliente a la sentenciaSQL
+        preparedStatement.setString(1, cliente.getNombre());
+        preparedStatement.setString(2, cliente.getApellido());
+        preparedStatement.setString(3, cliente.getCorreo());
+        preparedStatement.setLong(4, cliente.getIdentificacion());
+        preparedStatement.setLong(5, cliente.getTelefono());
+        preparedStatement.setString(6, cliente.getUsuario());
+        preparedStatement.setString(7, cliente.getContraseña());
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        preparedStatement.setString(8, formatDate.format(cliente.getFechaNacimiento()));
+        preparedStatement.setInt(9, cliente.getIdGenero());
+
+        preparedStatement.execute();
+    }
+
 }
